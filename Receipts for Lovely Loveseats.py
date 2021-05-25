c3758061fe9b3d88a6c453e35e46aca9f2e62d8c
Oussama <stack01engineer@gmail.com>
2021-05-25 20:12:20 +0100

    adding tax & printing the results

--- Receipts for Lovely Loveseats.py
+++ Receipts for Lovely Loveseats.py
@@ -11,8 +11,24 @@ luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Br
 
 luxurious_lamp_price = 52.15
 
+# variable that calculate sales tax
+sales_tax = .088
 
+# a variable running tally of customer expenses
+customer_one_total = 254.00
 
+# adding the price to the total price
+customer_one_total += luxurious_lamp_price
 
+# a variable that keeps a list of the descriptions of things they’re purchasing
+customer_one_itemization = "lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 
+# adding new item to the list
+customer_one_itemization += luxurious_lamp_description
 
+customer_one_tax = customer_one_total * sales_tax
+
+customer_one_total += customer_one_tax
+
+print("Customer One Items: " + customer_one_itemization + "\n")
+print("Customer One Total: ", customer_one_total)
